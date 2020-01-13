2572ed80022bd69c8012510a74e392521ad897de
laggingreflex <laggingreflex@gmail.com>
2020-01-13 10:15:38 +0530

    detect types, boolean->toggle, refactor

--- index.js
+++ index.js
@@ -4,20 +4,29 @@ enquirer.register('string', Enquirer.StringPrompt);
 enquirer.register('boolean', Enquirer.BooleanPrompt);
 enquirer.register('array', Enquirer.ArrayPrompt);
 
+const promptableTypes = ['string', 'number', 'boolean', 'array'];
+
 module.exports = new Proxy(base(), { get });
 
-function base(type = 'input') {
-  return async function EnquireSimple(opts, defaultValue) {
+function base(defaultType = 'input') {
+  return async function EnquireSimple(opts, initial) {
+    if (!opts) opts = {};
     if (typeof opts === 'string') opts = { message: opts };
-    const processChoice = processChoices(opts);
+    opts = { ...opts };
+    let processChoice;
+    [opts.choices, processChoice] = processChoices(opts.choices);
+    opts.initial = or(initial, opts.default, opts.initial);
+    opts.type = getType(opts.initial, or(opts.type, defaultType));
+    if (opts.type === 'boolean') {
+      opts.type = 'toggle'
+    }
+    if (opts.choices && !opts.type) {
+      opts.type = 'select';
+    }
+    opts.name = 'name';
     let answer;
     try {
-      const { name } = await enquirer.prompt({
-        type,
-        initial: defaultValue,
-        ...opts,
-        name: 'name',
-      });
+      const { name } = await enquirer.prompt(opts);
       answer = name;
     } catch (error) {
       if (error !== '') {
@@ -25,8 +34,8 @@ function base(type = 'input') {
         throw error;
       }
     }
-    if (answer === undefined && undefined !== defaultValue) {
-      return defaultValue;
+    if (answer === undefined && undefined !== initial) {
+      return initial;
     } else if (processChoice) {
       return processChoice(answer);
     } else {
@@ -39,13 +48,11 @@ function get(x, type) {
   return new Proxy(base(type), { get });
 }
 
-function processChoices(opts) {
-  const { choices } = opts;
-  if (!choices) return;
-  if (Array.isArray(choices)) return;
+function processChoices(choices) {
+  if (!choices) return [];
+  if (Array.isArray(choices)) return [choices];
   const keys = Object.keys(choices);
-  opts.choices = keys;
-  return async answer => {
+  return [keys, async answer => {
     const result = {};
     for (const key of arrify(answer)) {
       if (!(key in choices)) return;
@@ -61,9 +68,21 @@ function processChoices(opts) {
     } else {
       return result;
     }
-  }
+  }]
 }
 
 function arrify(array) {
   return Array.isArray(array) ? array : array === undefined ? [] : [array];
+}
+
+function getType(thing, defaultType = 'string') {
+  const type = Array.isArray(thing) ? 'array' : typeof thing;
+  if (promptableTypes.includes(type)) return type;
+  return defaultType
+}
+
+function or(...args) {
+  for (const arg of args) {
+    if (arg !== undefined) return arg;
+  }
 }
